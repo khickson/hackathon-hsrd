36d4186de11fcc3d85e3e529d149d4444f00fefe
tejbpatel <tejpatelbusiness@gmail.com>
2019-03-02 19:37:06 -0600

    v13

---
+++ searchForm.php
@@ -0,0 +1,106 @@
+<?php
+
+
+include ("dbconn.inc.php");
+$conn = dbConnect();
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+
+
+
+#myInput {
+  background-image: url('/css/searchicon.png');
+  background-position: 10px 12px;
+  background-repeat: no-repeat;
+  width: 100%;
+  font-size: 16px;
+  padding: 12px 20px 12px 40px;
+  border: 1px solid #ddd;
+  margin-bottom: 12px;
+}
+
+#myUL {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+#myUL li a {
+  border: 1px solid #ddd;
+  margin-top: -1px; /* Prevent double borders */
+  background-color: #f6f6f6;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px;
+  color: black;
+  display: block
+}
+
+#myUL li a:hover:not(.header) {
+  background-color: #eee;
+}
+</style>
+</head>
+<body>
+
+<h2>SEARCH</h2>
+
+<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">
+
+<ul id="myUL">
+
+<?php
+
+$sql = "SELECT * from Tags ORDER BY TAG_ID ASC";
+$stmt = $conn->stmt_init();
+if($stmt->prepare($sql)){
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($TAG_ID, $TAG_NAME);
+    while($stmt->fetch()){
+        echo "<li><a href='#'>$TAG_NAME</a></li>";
+    }
+    $stmt->close();
+}
+
+
+
+?>
+
+
+</ul>
+
+<script>
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+</script>
+
+</body>
+</html>
